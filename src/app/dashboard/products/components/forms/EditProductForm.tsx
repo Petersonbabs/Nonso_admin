3082bcea621b81f5def49812242816e993043849
petersonbabs <timiebabs@gmail.com>
2024-12-18 14:43:11 +0100

    dev

--- src/app/dashboard/products/components/forms/EditProductForm.tsx
+++ src/app/dashboard/products/components/forms/EditProductForm.tsx
@@ -15,10 +15,22 @@ interface EditProductFormProps {
     product: ProductProps;
 }
 
+interface FormPrice {
+    "1.4": number;
+    2: number;
+    4: number;
+    8: number;
+}
+
 const EditProductForm = ({ product }: EditProductFormProps) => {
-    const [formData, setFormData] = useState<Omit<ProductProps, "_id">>({
+    const [formData, setFormData] = useState<Omit<ProductProps, "_id"> & { price: FormPrice }>({
         name: "",
-        price: "",
+        price: {
+            1.4: 0,
+            2: 0,
+            4: 0,
+            8: 0,
+        },
         description: "",
         firstImage: null as unknown as File,
         secondImage: null as unknown as File,
@@ -62,7 +74,10 @@ const EditProductForm = ({ product }: EditProductFormProps) => {
         // Create a FormData object to handle file uploads
         const data = new FormData();
         data.append("name", formData.name);
-        data.append("price", formData.price);
+        data.append("price[1.4]", formData.price["1.4"].toString());
+        data.append("price[2]", formData.price[2].toString());
+        data.append("price[4]", formData.price[4].toString());
+        data.append("price[8]", formData.price[8].toString());
         data.append("description", formData.description);
         data.append("category", formData.category);
         if (formData.firstImage) data.append("firstImage", formData.firstImage);
@@ -87,15 +102,55 @@ const EditProductForm = ({ product }: EditProductFormProps) => {
                     </div>
 
                     <div>
-                        <Label>Price</Label>
-                        <Input
-                            name="price"
-                            value={formData.price}
-                            onChange={handleInputChange}
-                            placeholder="Price"
-                            type="number"
-                            required
-                        />
+                        <h2>Prices</h2>
+                        <div className="flex my-2 items-center h-[35px] border">
+                            <Label className="bg-black text-white h-full flex items-center justify-center p-2">1.4 ltr</Label>
+                            <Input
+                                className="flex-1 h-full"
+                                name="price.1.4"
+                                value={formData.price[1.4] === 0 ? undefined : formData.price[1.4]}
+                                onChange={handleInputChange}
+                                placeholder="Price for 1.4 litre"
+                                type="number"
+                                required
+                            />
+                        </div>
+                        <div className="flex my-2 items-center h-[35px] border">
+                            <Label className="bg-black text-white h-full flex items-center justify-center p-2">2 ltrs</Label>
+                            <Input
+                                className="flex-1 h-full"
+                                name="price.2"
+                                value={formData.price[2] === 0 ? undefined : formData.price[2]}
+                                onChange={handleInputChange}
+                                placeholder="Price for 2 litres"
+                                type="number"
+                                required
+                            />
+                        </div>
+                        <div className="flex my-2 items-center h-[35px] border">
+                            <Label className="bg-black text-white h-full flex items-center justify-center p-2">4 ltrs</Label>
+                            <Input
+                                className="flex-1 h-full"
+                                name="price.4"
+                                value={formData.price[4] === 0 ? undefined : formData.price[4]}
+                                onChange={handleInputChange}
+                                placeholder="Price for 4 litres"
+                                type="number"
+                                required
+                            />
+                        </div>
+                        <div className="flex my-2 items-center h-[35px] border">
+                            <Label className="bg-black text-white h-full flex items-center justify-center p-2">8 ltrs</Label>
+                            <Input
+                                className="flex-1 h-full"
+                                name="price.8"
+                                value={formData.price[8] === 0 ? undefined : formData.price[8]}
+                                onChange={handleInputChange}
+                                placeholder="Price for 8 litres"
+                                type="number"
+                                required
+                            />
+                        </div>
                     </div>
 
                     <div>
